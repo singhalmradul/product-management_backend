e84d8713ad21c1b99443fe764ccc343c09c9f98e
singhalmradul <singhalmradul@gmail.com>
2024-11-12 08:03:29 -0500

    remove audit columns

--- src/main/java/io/github/singhalmradul/product_management/model/entities/AuditableEntity.java
+++ src/main/java/io/github/singhalmradul/product_management/model/entities/AuditableEntity.java
@@ -1,34 +1,26 @@
 package io.github.singhalmradul.product_management.model.entities;
 
-import static java.time.LocalDateTime.now;
-
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Data;
 
 @Data
 @MappedSuperclass
 public abstract class AuditableEntity {
 
-    @JsonIgnore
-    private LocalDateTime createdAt;
+    // @JsonIgnore
+    // private LocalDateTime createdAt;
 
-    @JsonIgnore
-    private LocalDateTime updatedAt;
+    // @JsonIgnore
+    // private LocalDateTime updatedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = now();
-        updatedAt = now();
-    }
+    // @PrePersist
+    // protected void onCreate() {
+    //     createdAt = now();
+    //     updatedAt = now();
+    // }
 
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = now();
-    }
+    // @PreUpdate
+    // protected void onUpdate() {
+    //     updatedAt = now();
+    // }
 }
